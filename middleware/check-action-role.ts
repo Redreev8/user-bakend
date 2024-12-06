6087809de33caf5b20fba2ca9d2951dedf3a57b5
redreev8 <redreevs@mail.ru>
2024-12-06 23:18:26 +0500

    change check action token

--- middleware/check-action-role.ts
+++ middleware/check-action-role.ts
@@ -1,12 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
-import { findPayloadToken } from '../controller/token/token.model'
+import { findPayloadToken, findToken } from '../controller/token/token.model'
 import { User } from '../controller/users/users.model'
 
 const checkTokenAction = (
-    payload: User | string[] | string | null,
+    payload: User | string[] | string,
     url: string,
 ) => {
-    if (!payload) return
     if (typeof payload === 'string') {
         return payload === url || payload === 'ALL'
     }
@@ -16,26 +15,26 @@ const checkTokenAction = (
     return payload.actions.includes(url) || payload.actions.includes('ALL')
 }
 
-const checkActionRole = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-): Promise<any> => {
-    console.log(req)
-    const authToken = req.get('auth-token')
-    const token = req.get('token')
-    const payloaAuth = authToken ? await findPayloadToken(authToken) : null
-    const payload = token ? await findPayloadToken(token) : null
-    const request = `${req.method} ${req.path}`
-    if (
-        checkTokenAction(payloaAuth as string, request) ||
-        checkTokenAction(payload as string, request)
-    ) {
-        next()
+const checkActionRole = (
+    nameTokens: string[]
+): ((req: Request, res: Response, next: NextFunction,) => void) => {
+    return async (req, res, next) => {
+        const request = `${req.method} ${req.path}`
+        for (let i = 0; i < nameTokens.length; i++) {
+            const token = req.get(nameTokens[i])
+            if (!token) continue
+            const isTokenRedis = await findToken(token)
+            if (!isTokenRedis) continue
+            const payload = token ? await findPayloadToken(token) : null
+            if (!payload) continue
+            if (checkTokenAction(payload as string, request)) {
+                next()
+                return
+            }
+        }
+        res.status(400).json({ message: 'Ошибка токена' })
         return
     }
-    res.status(400).json({ message: 'Ошибка токена' })
-    return
 }
 
 export default checkActionRole
